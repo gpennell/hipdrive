d88d3e3b42ec91b493410ee55ac5f45908f2a354
Garrett Pennell <garrettpennell@gmail.com>
2023-11-03 11:49:06 -0500

    Added heading to warmup sets (#5)

--- src/Warmups/Warmups.tsx
+++ src/Warmups/Warmups.tsx
@@ -5,16 +5,19 @@ interface WarmupsProps {
 
 const Warmups: React.FC<WarmupsProps> = ({ sets, activeSet }) => {
   return (
-    <div role="list" className="warmup-sets">
-      {sets.map((set, index) => (
-        <button
-          key={index}
-          role="listitem"
-          aria-current={index === activeSet ? "true" : undefined}
-        >
-          {set}
-        </button>
-      ))}
+    <div className="warmupSets">
+      <h2>Warmup sets</h2>
+      <div role="list" className="warmup-sets">
+        {sets.map((set, index) => (
+          <button
+            key={index}
+            role="listitem"
+            aria-current={index === activeSet ? "true" : undefined}
+          >
+            {set}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
