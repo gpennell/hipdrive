15578e195392da400366381d4604ff264878937a
Garrett Pennell <garrettpennell@gmail.com>
2023-11-08 09:59:55 -0600

    Allow user to enter target weight

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import PlateDiagram from "./PlateDiagram/PlateDiagram";
 import Warmups from "./Warmups/Warmups";
+import WorkoutControl from "./WorkoutControl/WorkoutControl";
+import { addWeights, calcWarmupSets } from "./utils/WeightCalcs";
 
 function App() {
+  // Initial values
+  const [workSetWeight, setWorkSetWeight] = useState(45);
+  const [plates, setPlates] = useState<number[]>([]);
+  const [sets, setSets] = useState<number[]>([]);
+
+  const onGenerateWorkout = (newWeight: number) => {
+    setWorkSetWeight(newWeight);
+    setSets(calcWarmupSets(4, 45, 0.1, workSetWeight));
+    setPlates(addWeights(45, workSetWeight, [2.5, 5, 10, 25, 35, 45]));
+  };
+
   return (
     <>
-      <Warmups sets={[45, 150, 285]} activeSet={3} />
-      <PlateDiagram plates={[45, 45, 5]} />
+      <WorkoutControl
+        workSetWeight={workSetWeight}
+        onGenerateWorkout={onGenerateWorkout}
+      />
+      <Warmups sets={sets} activeSet={3} />
+      <PlateDiagram plates={plates} />
     </>
   );
 }
